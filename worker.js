ff052fe078207f4b2320dcd6d5e730b0fb9fe84f
LeNgocMinh2704 <20521600@gm.uit.edu.vn>
2023-11-01 15:07:47 +0700

    Update Build

--- worker.js
+++ worker.js
@@ -3,7 +3,16 @@ console.log("worker injection");
 self.addEventListener("push", function (e) {
     const data = e.data.json();
     console.log("Log check data", data);
-    self.registration.showNotification(data.title, {
-        body: data.body,
-    });
+    console.log("Log check self: ", self);
+    self.registration
+        .showNotification(data.title, {
+            body: data.body,
+        })
+        .then(() => {
+            // Send a message to the main thread to speak the notification text
+            self.clients.matchAll().then(all => all.forEach(client => {
+                client.postMessage(data.body);
+            }));
+        });
 });
+
